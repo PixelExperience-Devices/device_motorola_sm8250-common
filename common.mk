7d04511281eebdb08011f62f7815ae6fce855de8
Erfan Abdi <erfangplus@gmail.com>
2021-12-18 07:37:10 +0330

    sm8250-common: Add hotword priv app permissions
    Change-Id: Ic936e852cb77fe47c0ce69465b7a3717c2619b7e

--- common.mk
+++ common.mk
@@ -313,6 +313,10 @@ PRODUCT_PACKAGES += \
     android.hardware.health@2.1-impl.recovery \
     android.hardware.health@2.1-service
 
+# HotwordEnrollement
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/privapp-permissions-hotword.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-hotword.xml
+
 # IPACM
 PRODUCT_PACKAGES += \
     ipacm \
