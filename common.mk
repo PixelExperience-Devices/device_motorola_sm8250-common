3fef0894eede081f140629b863e1093941328be5
Jarl-Penguin <jarlpenguin@outlook.com>
2022-05-05 18:27:58 +0300

    sm8250-common: Enable quick tap
    Signed-off-by: Jarl-Penguin <jarlpenguin@outlook.com>

--- common.mk
+++ common.mk
@@ -131,6 +131,9 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.sip.voip.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.sip.voip.xml \
     frameworks/native/data/etc/android.software.vulkan.deqp.level-2020-03-01.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.vulkan.deqp.level.xml
 
+# Quick tap
+TARGET_SUPPORTS_QUICK_TAP := true
+
 # Atrace
 PRODUCT_PACKAGES += \
     android.hardware.atrace@1.0-service
