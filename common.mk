d540c7072d943cd5e7ee9b3afe44609d6bb9e797
johnmart19 <johnivan19999@gmail.com>
2022-05-06 02:09:56 +0300

    sm8250-common: Add compilation of legacy Audio for muting logspam
    - We are recieving errors on Init about this, so let's satisfy environment:
    [    7.888683] init: Command 'restart vendor.audio-hal-4-0-msd' action=onrestart (<Service 'audioserver' onrestart>:2) took 0ms and failed: service vendor.audio-hal-4-0-msd not found
    [    7.888698] init: Command 'restart audio_proxy_service' action=onrestart (<Service 'audioserver' onrestart>:3) took 0ms and failed: service audio_proxy_service not found
    [    7.888722] init: Command 'restart vendor.audio-hal-2-0' action=onrestart (<Service 'audioserver' onrestart>:4) took 0ms and failed: service vendor.audio-hal-2-0 not found
    [    7.888729] init: Command 'restart audio-hal-2-0' action=onrestart (<Service 'audioserver' onrestart>:5) took 0ms and failed: service audio-hal-2-0 not found
    
    Change-Id: Ia3917a2be7e789a64c15fb8642a463beb6974d16
    Signed-off-by: drkphnx <dark.phnx12@gmail.com>

--- common.mk
+++ common.mk
@@ -163,6 +163,11 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/default_volume_tables.xml:$(TARGET_COPY_OUT_VENDOR)/etc/default_volume_tables.xml \
     frameworks/av/services/audiopolicy/config/r_submix_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/r_submix_audio_policy_configuration.xml
 
+# Audio-Legacy for Muting logspam
+PRODUCT_PACKAGES += \
+    android.hardware.audio@2.0-impl \
+    android.hardware.audio@4.0-impl
+
 # Authsecret
 PRODUCT_PACKAGES += \
     android.hardware.authsecret@1.0.vendor
